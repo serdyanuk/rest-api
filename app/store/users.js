2e98ce610714021771b39ae410c982b048144c33
serdyanuk <plutoject@gmail.com>
2021-09-23 18:35:25 +0300

    code review

--- app/store/users.js
+++ app/store/users.js
@@ -5,11 +5,11 @@ const bcrypt = require('bcrypt');
 const config = require('../conifg');
 
 const addUser = async (login, password) => {
-  const [rows] = await getConnection().execute(
+  const [[user]] = await getConnection().execute(
     'SELECT id FROM users WHERE login = ?',
     [login]
   );
-  if (rows.length) {
+  if (user) {
     throw new Error('This login already exists');
   }
   const hash = await bcrypt.hash(password, config.PASSWORD_HASH_SALT);
