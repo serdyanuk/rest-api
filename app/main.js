02b3c7c7af70a6ac97b30e9766dff06f7dff3e0a
serdyanuk <plutoject@gmail.com>
2021-09-27 15:37:15 +0300

    add helmet

--- app/main.js
+++ app/main.js
@@ -3,8 +3,11 @@
 const store = require('./store');
 const express = require('express');
 const { items, users, auth } = require('./routes');
+const helmet = require('helmet');
+
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use([items, users, auth]);
 
