81d3d46a17300917d41408da75ccfb1bb1503d37
serdyanuk <plutoject@gmail.com>
2021-09-29 20:10:46 +0300

    add graceful shutdown

--- app/main.js
+++ app/main.js
@@ -14,17 +14,31 @@ app.use(express.json());
 app.use([items, users, auth]);
 app.use(errorHandler);
 
+let server = null;
+
 store
   .init()
   .then(() => {
-    app.listen(3000, () => logger.info('Server running on PORT: 3000'));
+    server = app.listen(3000, () =>
+      logger.info('HTTP server running on PORT: 3000')
+    );
   })
   .catch((e) => logger.error(e.message));
 
 const gracefulShutdown = () => {
   store.close();
+  if (server) {
+    server.close((err) => {
+      if (err) logger.error(err.message);
+
+      logger.info('HTTP server gracefuly shutdown');
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
 };
 
-// process.on('SIGINT', gracefulShutdown);
-// process.on('SIGTERM', gracefulShutdown);
-// process.on('SIGUSR2', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown); // ctrl+c
+process.on('SIGTERM', gracefulShutdown); // kill
+process.on('SIGUSR2', gracefulShutdown); // nodemon
