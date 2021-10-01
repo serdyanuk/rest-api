1075d3e49d3cf5308bae701ecff5dc3a8302d70c
serdyanuk <plutoject@gmail.com>
2021-10-01 21:18:23 +0300

    fix userLogin and userPassword joi validation

--- app/validations/user.js
+++ app/validations/user.js
@@ -2,8 +2,8 @@
 
 const Joi = require('joi');
 
-const userLoginRule = Joi.string().min(3).max(32);
-const userPassworRule = Joi.string().min(5).max(32);
+const userLoginRule = Joi.string().required().min(3).max(32);
+const userPassworRule = Joi.string().required().min(5).max(32);
 
 module.exports = {
   auth: Joi.object({
